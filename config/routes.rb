6b695656bff9b8e8b76db44f7506025ec9cbc74a
Tracey <traceyphan95@gmail.com>
2019-07-30 21:52:50 +0000

    Lessons

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root 'static_pages#index'
   resources :courses, only: [:index, :show]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :instructor do
+    resources :sections, only: [] do
+      resources :lessons, only: [:new, :create]
+    end
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:new, :create]
     end
-    resources :courses, only: [:new, :create, :show]
   end
 end
