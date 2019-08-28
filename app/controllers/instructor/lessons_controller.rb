e683912b726198513197cd871354570f2fd2d853
Tracey <traceyphan95@gmail.com>
2019-08-28 14:11:37 +0000

    Style the instructor page, moveable sections not complete

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -32,7 +32,7 @@ class Instructor::LessonsController < ApplicationController
 
   def require_authorized_for_current_section
     if current_section.course.user != current_user
-      return render plain: 'Unauthorized', status: :unauthorized
+      render plain: 'Unauthorized', status: :unauthorized
     end
   end
 
